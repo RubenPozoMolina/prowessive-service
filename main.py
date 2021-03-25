a7f0839c8cc457705bd0db6bd478965cca9b1114
Rubén Pozo <rubenpozomolina@gmail.com>
2021-03-25 21:27:57 +0100

    Added delete project

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def get_db():
         db.close()
 
 
-@app.post("/users/", response_model=schemas.User)
+@app.post("/users/", response_model=schemas.User, tags=["users"])
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
@@ -28,13 +28,13 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return crud.create_user(db=db, user=user)
 
 
-@app.get("/users/", response_model=List[schemas.User])
+@app.get("/users/", response_model=List[schemas.User], tags=["users"])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 
 
-@app.get("/users/{user_id}", response_model=schemas.User)
+@app.get("/users/{user_id}", response_model=schemas.User, tags=["users"])
 def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
@@ -42,20 +42,20 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
     return db_user
 
 
-@app.post("/users/{user_id}/items/", response_model=schemas.Item)
+@app.post("/users/{user_id}/items/", response_model=schemas.Item, tags=["users"])
 def create_item_for_user(
         user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
 ):
     return crud.create_user_item(db=db, item=item, user_id=user_id)
 
 
-@app.get("/items/", response_model=List[schemas.Item])
+@app.get("/items/", response_model=List[schemas.Item], tags=["users"])
 def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = crud.get_items(db, skip=skip, limit=limit)
     return items
 
 
-@app.post("/projects/", response_model=schemas.Project)
+@app.post("/projects/", response_model=schemas.Project, tags=["projects"])
 def create_project(project: schemas.ProjectCreate, db: Session = Depends(get_db)):
     db_project = crud.get_project(db, project.name)
     if db_project:
@@ -63,15 +63,21 @@ def create_project(project: schemas.ProjectCreate, db: Session = Depends(get_db)
     return crud.create_project(db=db, project=project)
 
 
-@app.get("/projects/", response_model=List[schemas.Project])
+@app.get("/projects/", response_model=List[schemas.Project], tags=["projects"])
 def read_projects(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     projects = crud.get_projects(db, skip=skip, limit=limit)
     return projects
 
 
-@app.get("/projects/{project_name}", response_model=schemas.Project)
-def read_project(project_name: str, db: Session = Depends(get_db)):
-    db_project = crud.get_project(db, project_name)
+@app.get("/projects/{project_id}", response_model=schemas.Project, tags=["projects"])
+def read_project(project_id: int, db: Session = Depends(get_db)):
+    db_project = crud.get_project(db, project_id)
     if db_project is None:
         raise HTTPException(status_code=404, detail="Project not found")
     return db_project
+
+
+@app.delete("/projects/{project_id}", response_model=schemas.Project, tags=["projects"])
+def delete_project(project_id: int, db: Session = Depends(get_db)):
+    project = crud.delete_project(db, project_id)
+    return project
